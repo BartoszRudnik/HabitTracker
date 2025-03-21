971db2da37200ca32c48217a5b0ab19eb9b05ff4
BartoszRudnik <Bartosz_Rudnik@outlook.com>
2025-03-21 15:54:52 +0100

    (feat) add create new habit button

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,11 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:habit_tracker/components/my_drawer.dart';
+import 'package:habit_tracker/database/habit_database.dart';
+import 'package:provider/provider.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends HookWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(drawer: MyDrawer(), appBar: AppBar());
+    final textController = useTextEditingController();
+
+    void createNewHabit() {
+      showDialog(
+        context: context,
+        builder:
+            (context) => AlertDialog(
+              content: TextField(
+                controller: textController,
+                decoration: InputDecoration(hintText: 'Create a new habit'),
+              ),
+              actions: [
+                MaterialButton(
+                  onPressed: () async {
+                    String newHabitName = textController.text.trim();
+
+                    context.read<HabitDatabase>().addHabit(newHabitName);
+
+                    textController.clear();
+
+                    Navigator.of(context).pop();
+                  },
+                  child: Text('Save'),
+                ),
+                MaterialButton(
+                  onPressed: () {
+                    textController.clear();
+
+                    Navigator.of(context).pop();
+                  },
+                  child: Text('Cancel'),
+                ),
+              ],
+            ),
+      );
+    }
+
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      drawer: MyDrawer(),
+      appBar: AppBar(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: createNewHabit,
+        elevation: 0,
+        backgroundColor: Theme.of(context).colorScheme.tertiary,
+        child: Icon(Icons.add),
+      ),
+    );
   }
 }
