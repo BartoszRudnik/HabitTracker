b8f4181d944fbdb0a19ddab98612d8d6783537b3
BartoszRudnik <Bartosz_Rudnik@outlook.com>
2025-03-21 15:15:51 +0100

    (refactor) extract drawer to seperate widget

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,21 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:habit_tracker/theme/theme_provider.dart';
-import 'package:provider/provider.dart';
+import 'package:habit_tracker/components/my_drawer.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      drawer: Drawer(
-        backgroundColor: Theme.of(context).colorScheme.surface,
-        child: Switch.adaptive(
-          value: Provider.of<ThemeProvider>(context).isDarkMode,
-          onChanged: (value) => Provider.of<ThemeProvider>(context, listen: false).toggleTheme(),
-        ),
-      ),
-      appBar: AppBar(),
-    );
+    return Scaffold(drawer: MyDrawer(), appBar: AppBar());
   }
 }
