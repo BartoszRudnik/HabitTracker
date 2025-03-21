c5d3beb60bcf958105408092ec1d3746feba5c52
BartoszRudnik <Bartosz_Rudnik@outlook.com>
2025-03-21 14:57:11 +0100

    (feat) add CRUD functionalities for habits

--- lib/database/habit_database.dart
+++ lib/database/habit_database.dart
@@ -28,4 +28,65 @@ class HabitDatabase extends ChangeNotifier {
 
     return settings?.firstLaunchDate;
   }
+
+  final List<Habit> currentHabits = [];
+
+  Future<void> addHabit(String habitName) async {
+    final newHabit = Habit()..name = habitName;
+
+    await isar.writeTxn(() => isar.habits.put(newHabit));
+
+    readHabits();
+  }
+
+  Future<void> readHabits() async {
+    List<Habit> fetchedHabits = await isar.habits.where().findAll();
+
+    currentHabits
+      ..clear()
+      ..addAll(fetchedHabits);
+
+    notifyListeners();
+  }
+
+  Future<void> updateHabitCompletion(int id, bool isCompleted) async {
+    final habit = await isar.habits.get(id);
+
+    if (habit != null) {
+      final today = DateTime.now();
+      final todayDate = DateTime(today.year, today.month, today.day);
+
+      await isar.writeTxn(() async {
+        if (isCompleted && !habit.completedDays.contains(todayDate)) {
+          habit.completedDays.add(todayDate);
+        } else {
+          habit.completedDays.removeWhere((date) => date == todayDate);
+        }
+
+        await isar.habits.put(habit);
+      });
+
+      readHabits();
+    }
+  }
+
+  Future<void> updateHabitName(int id, String newName) async {
+    final habit = await isar.habits.get(id);
+
+    if (habit != null) {
+      await isar.writeTxn(() async {
+        habit.name = newName;
+
+        await isar.habits.put(habit);
+      });
+
+      readHabits();
+    }
+  }
+
+  Future<void> deleteHabit(int id) async {
+    await isar.writeTxn(() async => await isar.habits.delete(id));
+
+    readHabits();
+  }
 }
